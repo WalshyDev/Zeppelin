5d2bf0e5429c12907c1734f776bdefa306867ba3
Daniel Walsh <walshydev@gmail.com>
2022-12-26 19:50:58 +0000

    Add channel_id to anyMessage trigger

--- backend/src/plugins/Automod/triggers/anyMessage.ts
+++ backend/src/plugins/Automod/triggers/anyMessage.ts
@@ -1,18 +1,30 @@
 import { Snowflake, TextChannel } from "discord.js";
 import * as t from "io-ts";
-import { verboseChannelMention } from "../../../utils";
+import { tNullable, verboseChannelMention } from "../../../utils";
 import { automodTrigger } from "../helpers";
 
 // tslint:disable-next-line:no-empty-interface
 interface AnyMessageResultType {}
 
 export const AnyMessageTrigger = automodTrigger<AnyMessageResultType>()({
-  configType: t.type({}),
+  configType: t.type({
+    channel_id: tNullable(t.string),
+  }),
 
-  defaultConfig: {},
+  defaultConfig: {
+    channel_id: null,
+  },
 
   async match({ pluginData, context, triggerConfig: trigger }) {
     if (!context.message) {
+      return;
+    }
+
+    if (trigger.channel_id) {
+      if (context.message.channel_id === trigger.channel_id) {
+        return { extra: { channel_id: trigger.channel_id } };
+      }
+
       return;
     }
 
